de928f91b2862562c42195487fe1e3fa144ecafa
Matt <mattsully01@gmail.com>
2019-06-15 23:50:24 -0600

    added ajax

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -1,4 +1,11 @@
 var topics = ['video games', 'dungeons and dragons', 'big bang theory','neil degrasse tyson']
+ 
+
+function initPage(){
+    displayButtons()
+}
+
+initPage()
 
 //  thid function adds buttons to the dom
 function displayButtons(){
@@ -6,8 +13,20 @@ function displayButtons(){
         var button = $('<button>');
         button.text(topics[i])
         button.appendTo('.buttons')
+        button.attr('data-search',topics[i])
         button.val(topics[i])
     }
+    $('button').on('click',function(){
+        var search = $(this).val();
+        var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=6t6phxrxukwcrB71KA15xTMFhQQPzBTf&q=" + search + "&limit=5&offset=0&rating=PG&lang=en"
+    
+        $.ajax({
+            url: queryURL,
+            method: "GET" }).then(function(response) {
+            console.log(response.data)
+        })
+    })
+    
 }
 
 //this function grabs user input from the text box and adds it to our array
@@ -19,9 +38,14 @@ $('#add-user-meme').on('click',function(event){
     //clears buttons to prevent array repeat
     $('.buttons').empty()
     displayButtons()
-
 })
 
 
 
-displayButtons()
+
+
+
+
+
+
+
